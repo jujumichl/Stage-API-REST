55e0f7905753b4e38a987051bdc90d09fa422198
Titouan GOINARD <titouan.goinard@lycee-basch.fr>
2025-12-09 17:57:41 +0100

    Correctif entity

--- app/src/Entity/Organisation.php
+++ app/src/Entity/Organisation.php
@@ -29,7 +29,7 @@ class Organisation
     #[ORM\Column(length: 100, nullable: true)]
     private ?string $ville = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(length: 50, nullable: true)]
     private ?string $tel = null;
 
     #[ORM\Column(length: 100, nullable: true)]
