7ef70d1a6e1697abe3d078d90d41fafa44245822
Julien MICHELIN <julien.michelin@lycee-basch.fr>
2025-12-09 18:07:07 +0100

    Correctif

--- app/bin/bdd.sh
+++ app/bin/bdd.sh
@@ -1,4 +1,3 @@
 bin/console doctrine:database:drop --force
 bin/console doctrine:database:create
 bin/console doctrine:schema:update --force
-bin/console doctrine:query:sql "$(<../db/realisation/stages_insertInto_v2.sql)"
