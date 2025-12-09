490db58132fdda40767224732ff4b22e550933db
Titouan GOINARD <titouan.goinard@lycee-basch.fr>
2025-12-09 18:01:14 +0100

    Correctif entity

--- app/src/Entity/Stage.php
+++ app/src/Entity/Stage.php
@@ -27,7 +27,7 @@ class Stage
     #[ORM\Column(type: Types::TEXT)]
     private ?string $descriptifMissions = null;
 
-    #[ORM\Column(type: Types::TEXT)]
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $moyens = null;
 
     #[ORM\ManyToOne]
