4c50aafeb5c02ef8b39c0342fab31371973fe247
Julien MICHELIN <julienmichelin35@gmail.com>
2026-03-17 16:37:34 +0100

    Fix du bin/bdd.sh

--- app/bin/bdd.sh
+++ app/bin/bdd.sh
@@ -1,4 +1,4 @@
 app/bin/console doctrine:database:drop --force
 app/bin/console doctrine:database:create
 app/bin/console doctrine:schema:update --force
-# app/bin/console doctrine:query:sql "$(<db/realisation/stages_insertinto_v2.sql)"
+# app/bin/console doctrine:query:sql "$(<db/realisation/stages_insertInto_v2.sql)"
