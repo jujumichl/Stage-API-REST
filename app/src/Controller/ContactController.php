7c96a6a34c5e8ebd63a60cf6fe25e6e22f56e86a
Titouan GOINARD <titouan.goinard@lycee-basch.fr>
2026-04-28 18:03:06 +0200

    Début du code des contacts d'une organisation

---
+++ app/src/Controller/ContactController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Entity\Organisation;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ContactRepository;
+use App\Repository\OrganisationRepository;
+use Symfony\Component\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\MakerBundle\Validator;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+final class ContactController extends AbstractController
+{
+    // Chemin pour voir tous les contacts
+    #[Route('/contacts', name: 'app_contact', methods: ['GET'])]
+    public function index(SerializerInterface $unSerializer, ContactRepository $unRepository): JsonResponse
+    {
+        $lesContacts = $unRepository->findAll();
+        $result = [
+            'message' => 'OK',
+            'data' => $lesContacts
+        ];
+        $serializedResult = $unSerializer->serialize($result, 'json', [AbstractNormalizer::IGNORED_ATTRIBUTES => ['civilite']]);
+        return new JsonResponse($serializedResult, JsonResponse::HTTP_OK, [], true);
+    }
+
+    // Chemin pour voir un contact selon l'ID
+     #[Route('/contacts/{id}', name: 'app_contact_id', methods: ['GET'])]
+     public function getContactId($id, SerializerInterface $unSerialiseur, ContactRepository $unRepository, ValidatorInterface $unValidateur)
+     {
+         $constraints = [
+             new Assert\NotBlank(),
+             new Assert\Regex(['pattern' => '/^[0-9]{1,8}$/', 'message' => "L'id doit comporter au minimum un et au maximum huit chiffres"]),
+         ];
+         $errors = $unValidateur->validate($id, $constraints);
+         if ($errors->count() > 0) {
+             $messages = [];
+             foreach ($errors as $error) {
+                 $messages[] = $error->getMessage();
+             }
+             return new JsonResponse([
+                 'message' => 'Données erronées',
+                 'errors' => $messages
+             ], JsonResponse::HTTP_BAD_REQUEST);
+         }
+         $unContact = $unRepository->find($id);
+         if (!$unContact) {
+             $result = [
+                 'message' => 'Ressource inexistante',
+                 'data' => null
+             ];
+             $serializedResult = $unSerialiseur->serialize($result, 'json');
+             return new JSONResponse($serializedResult, JsonResponse::HTTP_NOT_FOUND, [], true);
+         } else {
+             $result = [
+                 'message' => 'OK',
+                 'data' => $unContact
+             ];
+             $serializedResult = $unSerialiseur->serialize($result, 'json',  [AbstractNormalizer::IGNORED_ATTRIBUTES => ['civilite']]);
+             return new JSONResponse($serializedResult, JsonResponse::HTTP_OK, [], true);
+         }
+     }
+
+     // Chemin pour voir tous les contacts de l'organisation
+
+     // Il doit être modifier le code pour avoir les contacts de l'orga
+     #[Route('/organisation/{id}/contacts', name: 'app_organisation_contacts', methods: ['GET'])]
+     public function getContactsByOrganisation($id, SerializerInterface $unSerialiseur, OrganisationRepository $unRepository, ValidatorInterface $unValidateur)
+     {
+        $constraints = [
+             new Assert\NotBlank(),
+             new Assert\Regex(['pattern' => '/^[0-9]{1,8}$/', 'message' => "L'id doit comporter au minimum un et au maximum huit chiffres"]),
+         ];
+         $errors = $unValidateur->validate($id, $constraints);
+         if ($errors->count() > 0) {
+             $messages = [];
+             foreach ($errors as $error) {
+                 $messages[] = $error->getMessage();
+             }
+             return new JsonResponse([
+                 'message' => 'Données erronées',
+                 'errors' => $messages
+             ], JsonResponse::HTTP_BAD_REQUEST);
+         }
+         $uneOrganisation = $unRepository->find($id);
+         if (!$uneOrganisation) {
+             $result = [
+                 'message' => 'Ressource inexistante',
+                 'data' => null
+             ];
+             $serializedResult = $unSerialiseur->serialize($result, 'json');
+             return new JSONResponse($serializedResult, JsonResponse::HTTP_NOT_FOUND, [], true);
+         } else {
+             $result = [
+                 'message' => 'OK',
+                 'data' => $uneOrganisation
+             ];
+             $serializedResult = $unSerialiseur->serialize($result, 'json',  [AbstractNormalizer::IGNORED_ATTRIBUTES => ['civilite']]);
+             return new JSONResponse($serializedResult, JsonResponse::HTTP_OK, [], true);
+         }
+     }
+
+    //  #[Route('/contacts', name: 'app_contact_add', methods: ['POST'])]
+    //  public function addContact(
+    //      SerializerInterface $unSerialiseur,
+    //      ValidatorInterface $unValidateur,
+    //      Request $request,
+    //      EntityManagerInterface $em,
+    //      ContactRepository $unRepository,
+    //      URLGeneratorInterface $unUrlGenerateur
+    //  ): JsonResponse {
+    //      $data = $request->getContent();
+    //      $prenom = $data['prenom'];
+    //      $nom = $data['nom'];
+    //      $email = $data['email'];
+    //      $tel = $data['tel'];
+    //      $fonction = $data['fonction'];
+    //      $organisationId = $data['organisation_id'];
+    //      $repo = [
+    //          'prenom' => $em->getRepository(Organisation::class) -> find($organisationId),
+    //          'nom' => $em->getRepository(Organisation::class) -> find($organisationId),
+    //          'email' => $em->getRepository(Organisation::class) -> find($organisationId),
+    //          'tel' => $em->getRepository(Organisation::class) -> find($organisationId),
+    //          'fonction' => $em->getRepository(Organisation::class) -> find($organisationId),
+    //          'organisation' => $em->getRepository(Organisation::class) -> find($organisationId)
+    //      ];
+    //      $unContact = $unSerialiseur->deserialize($data, Contact::class, 'json');
+    //      $unContact->setNumeroOrganisation($repo['organisation']);        
+    //      $em->persist($unContact);
+    //      $em->flush();
+    //      $location = $unUrlGenerateur->generate('app_contact_add', ['id' => $unContact->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+    //      $result = [
+    //          'message' => "Contact d'id " . $unContact->getId() . " a été ajouté",
+    //          '_selfLink' => $location
+    //      ];
+    //      return new JsonResponse($result, JsonResponse::HTTP_OK, [], true);
+    //  }
+ }
